44031d2ae22d2bfa7fdd7ce86da54bf3acee5120
Duc Nguyen <nthienduc5@gmail.com>
2026-09-22 16:52:38 +0700

    update

--- Học máy/bai01/code/kiemtra.py
+++ Học máy/bai01/code/kiemtra.py
@@ -1,5 +1,2 @@
-import numpy, pandas, matplotlib, sklearn
-print("numpy ", numpy.__version__)
-print("pandas ", pandas.__version__)
-print("matplotlib ", matplotlib.__version__)
-print("scikit-learn", sklearn.__version__)
+import os
+print(os.getcwd())
